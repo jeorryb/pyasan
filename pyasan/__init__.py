cff9028f5a715eac799d1bedcaf1fa8a1bdef032
Jeorry Balasabas <jeorryb@Mac.attlocal.net>
2025-09-15 09:24:33 -0400

    Bump version to 0.3.0

--- pyasan/__init__.py
+++ pyasan/__init__.py
@@ -5,7 +5,7 @@ This package provides a simple interface to interact with NASA's various APIs,
 starting with the Astronomy Picture of the Day (APOD) API.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __author__ = "Jeorry Balasabas"
 __email__ = "jeorry@gmail.com"
 
